1516826e08fd329e4f3d0027d4f0db63955fe0c2
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2025-05-27 11:56:19 -0300

    tables

--- script_creación_inicial.sql
+++ script_creación_inicial.sql
@@ -78,3 +78,59 @@ CREATE TABLE detalle_pedido(
     CONSTRAINT FK_DetallePedido_Sillon FOREIGN KEY (det_sillon) 
         REFERENCES sillon(sill_id) -- Ajustar según tu nombre de tabla
 ) -- se complicó un poco pq es compuesta por 2 FK la PK :)
+
+CREATE TABLE proveedor(
+    prov_cuit NVARCHAR(255) NOT NULL PRIMARY KEY,
+    prov_razonSocial NVARCHAR(255),
+    prov_telefono NVARCHAR(255),
+    prov_mail NVARCHAR(255),
+    prov_direccion BIGINT,
+        CONSTRAINT FK_Proveedor_Direccion FOREIGN KEY (prov_direccion)
+            REFERENCES direccion(direc_id)
+)--chequear NOT NULLS
+
+CREATE TABLE compra(
+    comp_numero DECIMAL(18,0) NOT NULL PRIMARY KEY,
+    comp_fecha DATETIME2(6),
+    comp_total DECIMAL(18,2),
+    comp_sucursal BIGINT,
+    comp_proveedor NVARCHAR(255),
+
+        CONSTRAINT FK_Compra_Sucursal FOREIGN KEY (comp_sucursal)
+            REFERENCES sucursal(suc_numero)
+        CONSTRAINT FK_Compra_Proveedor FOREIGN KEY (comp_proveedor)
+            REFERENCES provedor(prov_cuit)
+)
+
+CREATE TABLE factura(
+    fact_numero BIGINT NOT NULL PRIMARY KEY,
+    fact_fecha DATETIME2(6),
+    fact_cliente BIGINT,
+    fact_sucursal BIGINT,
+
+        CONSTRAINT FK_Factura_Cliente FOREIGN KEY (fact_cliente) 
+            REFERENCES cliente(clie_id)
+        CONSTRAINT FK_Factura_Sucursal FOREIGN KEY (fact_sucursal)
+            REFERENCES sucursal(suc_numero)
+)
+
+CREATE TABLE envio(
+    env_numero DECIMAL(18,0) NOT NULL PRIMARY KEY,
+    env_fecha_programada DATETIME2(6),
+    env_fecha DATETIME2(6),
+    env_importe_traslado DECIMAL(18,2),
+    env_importeSubida DECIMAL(18,2),
+    env_factura BIGINT,
+
+        CONSTRAINT PK_Envio_Factura FOREIGN KEY (env_factura)
+            REFERENCES factura(fact_numero)
+)
+
+CREATE TABLE detalle_compra()
+CREATE TABLE modelo()
+CREATE TABLE detalle_factura()
+CREATE TABLE detalle_pedido()
+CREATE TABLE sillon()
+CREATE TABLE material()
+CREATE TABLE modelo()
+CREATE TABLE medida()
