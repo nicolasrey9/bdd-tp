b54cea75c4c7ce632e476674d06d0038ace807a9
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2025-05-30 16:47:22 -0300

    migrations

--- migracion2.sql
+++ migracion2.sql
@@ -27,19 +27,6 @@ BEGIN
                 (select prov_id from BASADOS.provincia where prov_nombre=Sucursal_Provincia)
         ) from gd_esquema.Maestra
 
-    INSERT BASADOS.cancelacion
-        (
-            canc_pedido,
-            canc_fecha,
-            canc_motivo
-        )
-        SELECT DISTINCT 
-        Pedido_Numero, 
-        Pedido_Cancelacion_Fecha, 
-        Pedido_Cancelacion_Motivo
-        FROM gd_esquema.Maestra 
-        WHERE Pedido_Numero IS NOT NULL
-
     INSERT BASADOS.proveedor(prov_cuit, prov_razon_social, prov_telefono, prov_mail, prov_direccion, prov_localidad)
         SELECT DISTINCT 
         Proveedor_Cuit, 
@@ -54,35 +41,6 @@ BEGIN
                 (select prov_id from BASADOS.provincia where prov_nombre=Proveedor_Provincia)
         ) from gd_esquema.Maestra where Proveedor_Cuit is not null
 
-    INSERT BASADOS.compra(comp_numero, comp_fecha, comp_total, comp_sucursal, comp_proveedor)
-        SELECT 
-        Compra_Numero,
-        Compra_Fecha,
-        Compra_Total,
-        Sucursal_NroSucursal,
-        Proveedor_Cuit        
-        FROM gd_esquema.Maestra 
-        where Compra_Numero is not null
-
-    INSERT BASADOS.detalle_compra
-        (
-            det_compra,
-            det_material,
-            det_precio_unitario,
-            det_cantidad
-        )
-        SELECT
-        Compra_Numero,
-        /*
-            aca se supone que va material, pero
-            me entro la duda si se usa mat_id
-
-        */
-        Detalle_Compra_Precio,
-        Detalle_Compra_Cantidad
-        FROM gd_esquema.Maestra 
-        WHERE Sillon_Modelo IS NOT NULL
-
     INSERT BASADOS.tipo_material(tipo_nombre)
         SELECT DISTINCT Material_Tipo 
         FROM gd_esquema.Maestra 
@@ -95,10 +53,8 @@ BEGIN
             FROM BASADOS.tipo_material 
             WHERE tipo_nombre = Material_Tipo
         ) 
-        FROM gd_esquema.Maestra 
-        
-        WHERE Material_Nombre is not NULL
-
+        FROM gd_esquema.Maestra WHERE Material_Nombre is not NULL
+    
     INSERT BASADOS.tela(tela_material, tela_color, tela_textura)
         SELECT DISTINCT 
             (
@@ -114,20 +70,16 @@ BEGIN
             ),
             Tela_Color,
             Tela_Textura
-        FROM gd_esquema.Maestra
-     
+        FROM gd_esquema.Maestra where Material_Tipo = 'Tela'
+
     INSERT BASADOS.relleno(rell_material, rell_densidad)
     SELECT DISTINCT
-    /*
-        creo que aca tendria que haber una vista tambien...
-    */
-
         (SELECT mat_id FROM BASADOS.material 
             WHERE mat_nombre = Material_Nombre 
             AND mat_tipo = (SELECT tipo_id FROM BASADOS.tipo_material WHERE tipo_nombre = Material_Tipo)
         ),
         Relleno_Densidad
-    FROM gd_esquema.Maestra
+    FROM gd_esquema.Maestra WHERE Material_Tipo = 'Relleno'
 
     INSERT BASADOS.madera(mad_material, mad_color, mad_dureza)
     SELECT DISTINCT
@@ -143,131 +95,75 @@ BEGIN
         ),
         Madera_Color,
         Madera_Dureza
-    FROM gd_esquema.Maestra
+    FROM gd_esquema.Maestra where Material_Tipo = 'Madera'
+    
+    INSERT BASADOS.cliente(clie_dni, clie_nombre, clie_apellido, clie_mail, clie_telefono, clie_fecha_nac, clie_direccion, clie_localidad)
+    SELECT DISTINCT Cliente_Dni, Cliente_Nombre, Cliente_Apellido, Cliente_Mail, 
+    Cliente_Telefono, Cliente_FechaNacimiento, Cliente_Direccion, 
+    (select local_id from BASADOS.localidad where local_nombre=Cliente_Localidad 
+    and local_provincia=(select prov_id from BASADOS.provincia where prov_nombre=Cliente_Provincia)) from gd_esquema.Maestra WHERE Cliente_Dni is not NULL
+    
+    INSERT BASADOS.pedido(ped_numero, ped_estado, ped_total, ped_sucursal, ped_cliente)
+    SELECT distinct Pedido_Numero, Pedido_Estado, Pedido_Total, 
+    Sucursal_NroSucursal,
+    (select clie_id from BASADOS.cliente where clie_dni=Cliente_Dni and clie_direccion=Cliente_Direccion)
+     FROM gd_esquema.Maestra WHERE Pedido_Numero is not NULL
+    
+    INSERT BASADOS.factura(fact_numero, fact_fecha, fact_cliente, fact_sucursal)
+    SELECT distinct Factura_Numero, Factura_Fecha, 
+    (select clie_id from BASADOS.cliente where clie_dni=Cliente_Dni
+    and clie_direccion=Cliente_Direccion
+    ), 
+    Sucursal_NroSucursal 
+    FROM gd_esquema.Maestra where Factura_Numero is not NULL
 
+    INSERT BASADOS.cancelacion(canc_pedido, canc_fecha, canc_motivo)
+    SELECT DISTINCT Pedido_Numero, Pedido_Cancelacion_Fecha, Pedido_Cancelacion_Motivo 
+    FROM gd_esquema.Maestra WHERE Pedido_Estado = 'Cancelado' AND Pedido_Cancelacion_Fecha IS NOT NULL AND Pedido_Cancelacion_Motivo IS NOT NULL
 
-    INSERT BASADOS.sillon 
-        (   
-            sill_codigo, 
-            sill_modelo,
-            sill_medida_alto,
-            sill_medida_ancho,
-            sill_medida_profundidad
-        )
-    SELECT DISTINCT
-        Sillon_Codigo,
-        (
-            SELECT mod_codigo 
-            FROM BASADOS.modelo 
-            WHERE mod_modelo = Sillon_Modelo
-        ),
-        Sillon_Medida_Alto,
-        Sillon_Medida_Ancho,
-        Sillon_Medida_Profundidad
-    FROM gd_esquema.Maestra
-    WHERE Sillon_Codigo IS NOT NULL
+    INSERT BASADOS.envio(env_numero, env_fecha_programada,env_fecha, env_importe_traslado, env_importe_subida, env_factura)
+    SELECT distinct Envio_Numero, Envio_Fecha_Programada, Envio_Fecha,
+    Envio_ImporteTraslado, Envio_importeSubida,
+    Factura_Numero FROM gd_esquema.Maestra where Envio_Numero is not null
 
-    INSERT BASADOS.material_por_sillon
-        (   mat_sillon,
-            mat_material
-        )
-    SELECT DISTINCT
-        (
-            SELECT mat_sillon 
-            FROM BASADOS.sillon 
-            WHERE mat_sillon = Sillon_Modelo_Codigo
-        ),
-        (
-            SELECT mat_id FROM BASADOS.material 
+    INSERT BASADOS.modelo(mod_codigo, mod_modelo, mod_descripcion, mod_precio_base)
+    SELECT distinct Sillon_Modelo_Codigo, Sillon_Modelo, 
+    Sillon_Modelo_Descripcion, Sillon_Modelo_Precio from gd_esquema.Maestra
+    where Sillon_Modelo_Codigo is not null
+
+    INSERT BASADOS.medida(med_alto, med_ancho, med_profundidad, med_precio)
+    SELECT distinct Sillon_Medida_Alto, Sillon_Medida_Ancho, Sillon_Medida_Profundidad,
+    Sillon_Medida_Precio from gd_esquema.Maestra
+    where Sillon_Medida_Alto is not null
+
+    INSERT BASADOS.detalle_compra(det_compra, det_material, det_precio_unitario, det_cantidad)
+    SELECT Compra_Numero, (
+            SELECT mat_id 
+            FROM BASADOS.material 
             WHERE mat_nombre = Material_Nombre 
-            AND mat_tipo =
+            AND mat_tipo = 
                 (   SELECT tipo_id 
                     FROM BASADOS.tipo_material 
                     WHERE tipo_nombre = Material_Tipo
                 )
-        )
+        ),
+     Detalle_Compra_Precio, Detalle_Compra_Cantidad FROM gd_esquema.Maestra WHERE Compra_Numero IS NOT NULL
 
-    INSERT BASADOS.modelo
-    (   mod_codigo,
-        mod_modelo,
-        mod_descripcion,
-        mod_precio_base
-    )
-    SELECT 
-        Sillon_Modelo_Codigo,
-        Sillon_Modelo,
-        Sillon_Modelo_Descripcion,
-        Sillon_Modelo_Precio
-    FROM gd_esquema.Maestra
-    WHERE Sillon_Modelo_Codigo IS NOT NULL
+    INSERT BASADOS.sillon(sill_codigo, sill_modelo, sill_medida_alto, sill_medida_ancho, sill_medida_profundidad)
+    SELECT distinct Sillon_Codigo, Sillon_Modelo_Codigo, Sillon_Medida_Alto,
+    Sillon_Medida_Ancho, Sillon_Medida_Profundidad FROM gd_esquema.Maestra
+    WHERE Sillon_Codigo is not null
 
-    INSERT BASADOS.medida
-    (
-        med_alto,
-        med_ancho,
-        med_profundidad,
-        med_precio
-    )
-    SELECT 
-        Sillon_Medida_Alto,
-        Sillon_Medida_Ancho,
-        Sillon_Medida_Profundidad,
-        Sillon_Medida_Precio
-    FROM gd_esquema.Maestra
-
-    INSERT BASADOS.cliente
-        (
-            clie_id,
-            clie_dni,
-            clie_nombre,
-            clie_apellido,
-            clie_mail,
-            clie_telefono,
-            clie_fecha_nac,
-            clie_direccion,
-            clie_localidad
-        )
-    SELECT 
-        Cliente_Dni,
-        Cliente_Nombre,
-        Cliente_Apellido,
-        Cliente_Mail,
-        Cliente_Telefono,
-        Cliente_FechaNacimiento,
-        Cliente_Direccion,
-        
-        /*
-        creo que podrias crear una vista aca...
-        algo como:
-
-        CREATE VIEW vw_localidad_resuelta AS
-        SELECT
-                *,
-                l.local_id AS cliente_localidad_id
-        FROM gd_esquema.Maestra JOIN BASADOS.localidad l
-            ON Cliente_Localidad = l.local_nombre
-            AND l.local_provincia = 
-                (
-                    SELECT prov_id FROM BASADOS.provincia 
-                    WHERE prov_nombre = m.Cliente_Provincia
-                )
-        
-        y luego llamar a la vista como:
-            cliente_localidad_id
-            FROM vw_localidad_resuelta
-            WHERE Cliente_Dni IS NOT NULL
-        
-        */
-        
-        (SELECT local_id FROM BASADOS.localidad 
-            WHERE Cliente_Localidad = local_nombre 
-            AND local_provincia = 
-            (SELECT prov_id FROM BASADOS.provincia WHERE prov_nombre = Cliente_Provincia)
-        )
-    FROM gd_esquema.Maestra
-    WHERE /*se supone que seria el clie_id no? */ 
-        Cliente_Dni IS NOT NULL
-    IS NOT NULL
+    INSERT BASADOS.material_por_sillon(mat_sillon, mat_material)
+    SELECT Sillon_Codigo, (SELECT mat_id 
+            FROM BASADOS.material 
+            WHERE mat_nombre = Material_Nombre 
+            AND mat_tipo = 
+                (   SELECT tipo_id 
+                    FROM BASADOS.tipo_material 
+                    WHERE tipo_nombre = Material_Tipo
+                )), Material_Descripcion FROM gd_esquema.Maestra WHERE Sillon_Codigo IS NOT NULL
+END              
 GO
 
 
