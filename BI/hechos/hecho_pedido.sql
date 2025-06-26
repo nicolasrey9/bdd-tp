06c54aa0e44f91c35a91999d08f63c341f2897de
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2025-06-26 19:59:38 -0300

    indexes

--- BI/hechos/hecho_pedido.sql
+++ BI/hechos/hecho_pedido.sql
@@ -10,3 +10,7 @@ CREATE TABLE BASADOS.BI_Hecho_Pedido (
     CONSTRAINT FK_Tiempo FOREIGN KEY (tiempo_id) REFERENCES BASADOS.BI_Dim_Tiempo(tiempo_id),
     CONSTRAINT FK_Estado FOREIGN KEY (estado_id) REFERENCES BASADOS.BI_Dim_EstadoPedido(estado_id)
 );
+CREATE INDEX IX_HechoPedido_Tiempo ON BASADOS.BI_Hecho_Pedido(tiempo_id);
+CREATE INDEX IX_HechoPedido_Sucursal ON BASADOS.BI_Hecho_Pedido(suc_id);
+CREATE INDEX IX_HechoPedido_Turno ON BASADOS.BI_Hecho_Pedido(turno_id);
+CREATE INDEX IX_HechoPedido_Estado ON BASADOS.BI_Hecho_Pedido(estado_id);
