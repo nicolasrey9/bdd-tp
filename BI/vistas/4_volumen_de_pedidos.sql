c15d83e1a794038837f5eaf7f5f8ce25719a84c8
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2025-06-26 20:28:41 -0300

    views 4 y 5

--- BI/vistas/4_volumen_de_pedidos.sql
+++ BI/vistas/4_volumen_de_pedidos.sql
@@ -1,22 +1,21 @@
---Vista 4?
-CREATE VIEW BASADOS.BI_Volumen_Pedidos AS
+--Vista 4
+CREATE VIEW BASADOS.BI_VOLUMEN_PEDIDOS AS
 SELECT 
-    t.anio AS Año,
-    t.cuatrimestre AS Cuatrimestre,
-    t.mes AS Mes,
-    s.suc_numero AS Sucursal,
-    tu.descripcion AS Turno,
-    COUNT(p.pedido_numero) AS Cantidad_Pedidos
+    t.anio Anio,
+    t.mes Mes,
+    s.suc_numero NumeroSucursal,
+    tu.descripcion Turno,
+    COUNT(*) as CantidadPedidos
 FROM 
     BASADOS.BI_Hecho_Pedido p
     JOIN BASADOS.BI_Dim_Tiempo t ON p.tiempo_id = t.tiempo_id
     JOIN BASADOS.BI_Dim_Sucursal s ON p.suc_id = s.suc_id
     JOIN BASADOS.BI_Dim_Turno tu ON p.turno_id = tu.turno_id
 GROUP BY 
-    t.anio, t.cuatrimestre, t.mes, s.suc_numero, tu.descripcion
-
+    t.anio, t.mes, s.suc_numero, tu.descripcion
 GO
-select * from BASADOS.BI_Volumen_Pedidos
 
-drop view BASADOS.volumen_de_pedidos
+-- select * from BASADOS.BI_VOLUMEN_PEDIDOS
+
+-- drop view BASADOS.BI_VOLUMEN_PEDIDOS
 go
--- BI/vistas/5_conversion_de_pedidos.sql
+++ BI/vistas/5_conversion_de_pedidos.sql
@@ -1,14 +1,21 @@
---Vista 5 ?
-CREATE VIEW BASADOS.conversion_de_pedidos AS
+--Vista 5
+CREATE VIEW BASADOS.BI_CONVERSION_PEDIDOS AS
 SELECT 
-(count(p.pedido_numero) / (select count(pedido_numero) from BASADOS.BI_Hecho_Pedido)) PorcentajeDePedidosSegunEstado,
-ep.ped_estado Estado,
-concat(t.anio,t.cuatrimestre) CuatrimestreDelAnio,
-p.suc_numero Sucursal
+    t.anio Anio,
+    t.cuatrimestre Cuatrimestre,
+    s.suc_numero NumeroSucursal,
+    e.ped_estado EstadoPedido,
+    COUNT(*) as Cantidad,
+    (COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (PARTITION BY t.anio, t.cuatrimestre, s.suc_numero)) as PorcentajeDelTotal
+FROM 
+    BASADOS.BI_Hecho_Pedido p
+    JOIN BASADOS.BI_Dim_Tiempo t ON p.tiempo_id = t.tiempo_id
+    JOIN BASADOS.BI_Dim_Sucursal s ON p.suc_id = s.suc_id
+    JOIN BASADOS.BI_Dim_EstadoPedido e ON p.estado_id = e.estado_id
+GROUP BY 
+    t.anio, t.cuatrimestre, s.suc_numero, e.ped_estado
+GO
 
-from BASADOS.BI_Hecho_Pedido p
-join BASADOS.BI_Dim_Estado_Pedido ep on ep.ped_estado = p.ped_estado
-join BASADOS.BI_Dim_Tiempo t on t.tiempo_id = p.tiempo_id
+-- SELECT * from BASADOS.BI_CONVERSION_PEDIDOS
 
-GROUP by ep.ped_estado, t.cuatrimestre,p.suc_numero
-ORDER by t.anio,t.cuatrimestre
+-- DROP VIEW BASADOS.BI_CONVERSION_PEDIDOS
