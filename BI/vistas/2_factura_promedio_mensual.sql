cd376d7f6de169382b5fb636515bde70a488d89f
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2025-07-10 20:00:53 -0300

    .

--- BI/vistas/2_factura_promedio_mensual.sql
+++ BI/vistas/2_factura_promedio_mensual.sql
@@ -33,11 +33,12 @@ JOIN BASADOS.BI_Dim_Sucursal s ON v.suc_id = s.suc_id
 GROUP BY s.provincia, t.anio, t.cuatrimestre;
 GO
 
+---esta iria
 SELECT
     t.anio,
     t.mes,
     s.provincia,
-    avg(v.valor_total_ventas)
+    avg(v.valor_promedio_ventas)
 FROM BASADOS.BI_Hecho_Venta v
 JOIN BASADOS.BI_Dim_Tiempo t ON v.tiempo_id = t.tiempo_id
 JOIN BASADOS.BI_Dim_Sucursal s ON v.suc_id = s.suc_id
